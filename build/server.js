d8e72ebf6034a8b2aff24dfd51bf695bf9699b42
Luiz Andia <andia.luiz@gmail.com>
2019-08-12 17:12:42 -0300

    Heroku part

--- build/server.js
+++ build/server.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = express_1.default();
 app.get("/", (req, res, next) => {
-    res.send("Tour Booking API");
+    res.send("Tour Booking API HEHE");
 });
 app.get("/tours", (req, res, next) => {
     res.send("Get a list of tours...");
